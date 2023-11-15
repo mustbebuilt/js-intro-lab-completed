ce6c67e81f47fb9696efab11a331f42063e82616
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2023-11-15 08:59:28 +0000

    Banner Tidy

--- js/main.js
+++ js/main.js
@@ -1,16 +1,4 @@
 (() => {
-  // Color Picker
-  const colourButtons = document.querySelectorAll(".colPicker");
-
-  const chgColour = (ev) => {
-    const colourPicked = ev.target.classList[0] + "Back";
-    const bodyElement = document.body;
-    bodyElement.className = colourPicked === "resetBack" ? "" : colourPicked;
-  };
-
-  colourButtons.forEach((button) =>
-    button.addEventListener("click", chgColour)
-  );
 
   // Image Slider
   const images = [
