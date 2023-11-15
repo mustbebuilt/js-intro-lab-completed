11edad767fdb2004b0c7e5bd62ca9c32a50010ea
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2023-11-15 08:55:40 +0000

    Commented refactoring

--- js/exchange-v2.js
+++ js/exchange-v2.js
@@ -1,17 +1,28 @@
-(function () {
+(() => {
+  // Get input and output elements from the DOM
   const poundsIO = document.getElementById("pounds");
   const euroIO = document.getElementById("euro");
   const msg = document.getElementById("msg");
+
+  // Get all input buttons with type "button"
   const conversionBtns = document.querySelectorAll("input[type=button]");
+
+  // Display the properties of the conversionBtns in the console
   console.dir(conversionBtns);
+
+  // Add click event listeners to each conversion button
   conversionBtns.forEach(function (button) {
     button.addEventListener("click", function (ev) {
+      // Call the exchange function with the clicked button's id
       exchange(ev.target.id);
     });
   });
 
+  // Function to perform the currency exchange
   function exchange(id) {
     let inputBox, outputBox, inVal, outVal, exchangeRate;
+
+    // Determine input and output elements based on the clicked button's id
     switch (id) {
       case "p2e":
         inputBox = poundsIO;
@@ -24,21 +35,30 @@
         exchangeRate = 0.89;
         break;
     }
+
+    // Initialize output value to 0 and get the input value as a float
     outVal = 0;
     inVal = parseFloat(inputBox.value);
+
+    // Check if the input is not a valid number
     if (isNaN(inVal)) {
       msg.style.display = "block";
       msg.innerHTML = "You must enter a number";
       msg.setAttribute("class", "error");
     } else {
+      // Clear error message
       msg.innerHTML = "";
+
+      // Check if the input is greater than zero
       if (inVal === 0) {
         msg.style.display = "block";
         msg.innerHTML = "You must enter more than zero.";
         msg.setAttribute("class", "error");
       } else {
+        // Perform the currency exchange and display the result
         outVal = inVal * exchangeRate;
         msg.style.display = "block";
+        // Format the output value to two decimal places
         outVal = outVal.toFixed(2);
         outputBox.value = outVal;
       }
--- js/exchange.js
+++ js/exchange.js
@@ -1,34 +1,56 @@
-(function () {
+(() => {
+  // Variables to store the converted prices
   let pricePounds;
-  let exchangeRate;
   let priceEuros;
+
+  // Exchange rate for converting pounds to euros
+  const exchangeRate = 1.12;
+
+  // Get the form and message elements from the DOM
   let convertForm = document.getElementById("myForm");
+  let poundsInput = document.getElementById("pounds");
   let msg = document.getElementById("msg");
-  convertForm.addEventListener("submit", function (ev) {
+
+  // Add a submit event listener to the form
+  convertForm.addEventListener("submit", (ev) => {
+    // Prevent the default form submission
     ev.preventDefault();
+
+    // Reset the euros price to 0
     priceEuros = 0;
-    exchangeRate = 1.12;
-    pricePounds = parseFloat(document.getElementById("pounds").value);
+
+    // Get the input value for pounds and convert it to a floating-point number
+    pricePounds = parseFloat(poundsInput.value);
+
+    // Check if the entered value is not a valid number
     if (isNaN(pricePounds)) {
+      // Display an error message if the input is not a number
       msg.style.display = "block";
       msg.innerHTML = "You must enter a number";
       msg.setAttribute("class", "error");
     } else {
+      // Check if the entered value is greater than zero
       if (pricePounds === 0) {
+        // Display an error message if the input is zero
         msg.style.display = "block";
         msg.innerHTML = "You must enter more than zero.";
         msg.setAttribute("class", "error");
       } else {
+        // Calculate the euros price and display the result
         priceEuros = pricePounds * exchangeRate;
         msg.style.display = "block";
+        // Format the euros price to two decimal places
         priceEuros = priceEuros.toFixed(2);
-        msg.innerHTML = "You will get &euro;" + priceEuros;
+        msg.innerHTML = `You will get &euro;${priceEuros}`;
         msg.removeAttribute("class");
       }
     }
   });
-  document.getElementById("pounds").addEventListener("focus", function () {
-    this.value = "";
+
+  // Add a focus event listener to the pounds input
+  poundsInput.addEventListener("focus", () => {
+    // Clear the input value and reset the message
+    poundsInput.value = "";
     msg.innerHTML = "";
     msg.removeAttribute("class");
   });
