da9222107e7ff29e3ed49cb31dc9820c69641310
mustbebuilt <martin@mustbebuilt.co.uk>
2024-07-24 14:26:17 +0100

    Arrows Added

--- js/basicBanner.js
+++ js/basicBanner.js
@@ -3,25 +3,26 @@
   const nextBtn = document.getElementById("nextBtn");
   const slides = document.querySelector(".slides");
   let currentIndex = 0;
-  // events
-  prevBtn.addEventListener("click", prevSlide);
-  nextBtn.addEventListener("click", nextSlide);
+
   // functions
-  function showSlide(index) {
+  const showSlide = (index) => {
     slides.style.transform = `translateX(-${index * 100}%)`;
-  }
-  function prevSlide() {
+  };
+  const prevSlide = () => {
     currentIndex =
       (currentIndex - 1 + slides.children.length) % slides.children.length;
     showSlide(currentIndex);
-  }
-  function nextSlide() {
+  };
+  const nextSlide = () => {
     currentIndex = (currentIndex + 1) % slides.children.length;
     showSlide(currentIndex);
-  }
+  };
   // cycle
-  function autoSlide() {
-    nextSlide();
-  }
+  const autoSlide = () => nextSlide();
+
+  // events
+  prevBtn.addEventListener("click", prevSlide);
+  nextBtn.addEventListener("click", nextSlide);
+
   setInterval(autoSlide, 5000);
 })();
